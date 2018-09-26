82c7ad1039fbae80a48303de960fef3d9e091a6e
Viet Vo <viet.allen.qvo@gmail.com>
2018-09-25 20:08:47 -0700

    initial mvc file structure and db created

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Meeses', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bobbi', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Sylvester', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Marilyn Manson', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Joe Biden', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Owens', false);
