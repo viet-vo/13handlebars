4115cdb8c3516a85f7860f57e16f58c51ccf384b
Viet Vo <viet.allen.qvo@gmail.com>
2018-09-28 00:49:46 -0700

    init heroku deploy1

---
+++ public/assets/js/burgers.js
@@ -0,0 +1,39 @@
+$(function () {
+    $(".change-devoured").on("click", function (event) {
+        var id = $(this).data("id");
+        var newDevour = $(this).data("newdevour");
+
+        var newDevourState = {
+            devoured: newDevour
+        };
+
+        $.ajax("/api/burgers/" + id, {
+            type: "PUT",
+            data: newDevourState
+        }).then(
+            function () {
+                console.log("changed devoured to", newDevour);
+                location.reload();
+            }
+        );
+    });
+
+    $(".create-form").on("submit", function (event) {
+        event.preventDefault();
+
+        var newBurger = {
+            name: $("#burger").val().trim(),
+            devoured: $("[name=devoured]:checked").val().trim()
+        };
+
+        $.ajax("/api/burgers", {
+            type: "POST",
+            data: newBurger
+        }).then(
+            function () {
+                console.log("created new burger");
+                location.reload();
+            }
+        );
+    });
+});
